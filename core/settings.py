78c9808474bced2e2183ed97b2ff80e82e25e44f
Vinícius <viniscrv@gmail.com>
2024-07-17 16:41:27 -0300

    feat: more deploy config

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import environ
+import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()
@@ -94,15 +95,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 if env.get_value("DB_NAME"):
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': env("DB_NAME"),
-            'USER': env("DB_USERNAME"),
-            'PASSWORD': env("DB_PASSWORD"),
-            'HOST': env("DB_HOST"),
-            'PORT': env("DB_PORT"),
-
-        }
+        'default': dj_database_url.parse(env("DB_URL"))
     }
 else:
     DATABASES = {
